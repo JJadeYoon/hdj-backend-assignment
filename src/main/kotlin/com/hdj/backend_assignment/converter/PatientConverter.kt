2d7cbbdbc19acf981b9724584a45ec401f382d40
JS Yoon <js.yoon@hdjunction.co.kr>
2025-01-28 19:18:34 +0900

    feat: PatientConverter 환자가입 요청 처리 구현

--- src/main/kotlin/com/hdj/backend_assignment/converter/PatientConverter.kt
+++ src/main/kotlin/com/hdj/backend_assignment/converter/PatientConverter.kt
@@ -1,17 +1,32 @@
 package com.hdj.backend_assignment.converter
 
+import com.hdj.backend_assignment.domain.Hospital
 import com.hdj.backend_assignment.domain.Patient
+import com.hdj.backend_assignment.global.enums.SexCode
 import com.hdj.backend_assignment.web.dto.PatientRequestDTO
 import com.hdj.backend_assignment.web.dto.PatientResponseDTO
 
 class PatientConverter {
     companion object {
         fun toRegisterResultDTO(patient:Patient): PatientResponseDTO.RegisterResultDTO {
-            TODO()
+            return PatientResponseDTO.RegisterResultDTO(
+                patientId = patient.id,
+                hospitalId = patient.hospital.id,
+                patientName = patient.patientName,
+                registrationNumber = patient.registrationNumber
+            )
         }
 
-        fun toPatient(request: PatientRequestDTO.RegisterDTO): Patient {
-            TODO()
+        fun toPatient(request: PatientRequestDTO.RegisterDTO, hospital: Hospital): Patient {
+            return Patient(
+                id = request.id,
+                hospital = hospital,
+                patientName = request.patientName,
+                registrationNumber = request.registrationNumber,
+                sexCode = SexCode.from(request.sexCode!!),
+                birthDate = request.birthDate,
+                phoneNumber = request.phoneNumber
+            )
         }
     }
 }
