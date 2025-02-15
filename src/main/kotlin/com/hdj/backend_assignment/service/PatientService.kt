4d6ab8dba436d163729e66236ee819e7d8bddbca
JJadeYoon <yjs19990421@gmail.com>
2025-02-15 15:43:15 +0900

    refac: 환자 등록 컨트롤러 수정 - 컨트롤러에서는 DTO만을 사용하도록 환자 서비스 로직 수정

--- src/main/kotlin/com/hdj/backend_assignment/service/PatientService.kt
+++ src/main/kotlin/com/hdj/backend_assignment/service/PatientService.kt
@@ -2,7 +2,8 @@ package com.hdj.backend_assignment.service
 
 import com.hdj.backend_assignment.domain.Patient
 import com.hdj.backend_assignment.web.dto.PatientRequestDTO
+import com.hdj.backend_assignment.web.dto.PatientResponseDTO
 
 interface PatientService {
-    fun registerPatient(request: PatientRequestDTO.RegisterDTO): Patient
+    fun registerPatient(request: PatientRequestDTO.RegisterDTO): PatientResponseDTO.RegisterResultDTO
 }
